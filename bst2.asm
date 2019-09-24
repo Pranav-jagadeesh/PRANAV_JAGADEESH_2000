53b430dcb2034d52bf9204a25289abe9532c8001
Pranav-jagadeesh <53830306+Pranav-jagadeesh@users.noreply.github.com>
2019-09-25 00:29:46 +0530

    PRANAV_JAGADEESH_2000

---
+++ bst2.asm
@@ -0,0 +1,26 @@
+;bublle sort in descending order
+.model small
+.data
+ array dw 5,-2,7,-4
+.code
+ mov ax,@data
+ mov ds,ax
+ mov dx,04
+ dec dx
+outer:mov cx,dx
+      mov SI,offset array
+again:mov ax,[SI]
+      cmp ax,[SI+2]
+      JG l1
+      XCHG ax,[SI+2]
+      XCHG ax,[SI]
+      l1:inc SI
+         inc SI
+         dec cx
+         jnz again
+         dec dx
+         jnz outer
+         mov ah,4ch
+         int 21h
+         end
+
